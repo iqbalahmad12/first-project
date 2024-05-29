3db7ead60aa8748a295c034121f95e1e73d33d5c
Your Name <you@example.com>
2024-05-29 11:21:23 +0530

    Add three pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,11 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "hero-pattern": "url('/src/img/Banner.jpg')",
-        "hero-pattern2": "url('/src/img/Call-to-action.jpg')",
-        "about-hero": "url('/src/img/about-banner.jpg')",
-        "about-hero2": "url('/src/img/talk-to-us.jpg')",
-        "contact-hero": "url('/src/img/contact-Banner.jpg')",
+        "hero-pattern": "url('./../src/img/Banner.jpg')",
+        "hero-pattern2": "url('./../src/img/Call-to-action.jpg')",
+        "about-hero": "url('./../src/img/about-banner.jpg')",
+        "about-hero2": "url('./../src/img/talk-to-us.jpg')",
+        "contact-hero": "url('./../src/img/contact-Banner.jpg')",
       },
     },
   },
